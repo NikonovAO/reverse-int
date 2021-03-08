74f8a29e4929b602547def439bf5fdf5467c8210
NikonovAO <nikonow@bk.ru>
2021-03-08 23:33:27 +0400

    feat: реализована функция, выводящая число в обратном порядке

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0)
+    {
+        n=n*(-1);
+    }
+    let n_to_String =  n.toString();
+    let array_to_num = n_to_String.split("");
+    let n_reverse = array_to_num.reverse("");
+    return n_reverse.join('');
 }
